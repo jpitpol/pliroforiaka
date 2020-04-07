a83d88e056eddded1afe6b20484742e5eca1366e
jpitpol <56702951+jpitpol@users.noreply.github.com>
2020-04-07 17:35:48 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,1 +1,16 @@
-python lab
+
+def removeDuplicates(mylist):
+    mylist = list(dict.fromkeys(mylist))
+    return mylist
+
+
+def sortList(mylist):
+    mylist.sort()
+    return mylist
+
+a_dict= {"a":10, "b":12, "c":14, "d":14, "e":16, "f":28, "g":28, "h":30}
+a_dict = removeDuplicates(a_dict)
+print(a_dict)
+
+a_dict = sortList(a_dict)
+print(a_dict)
